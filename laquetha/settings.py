6e9067cd10616b98220688f51c361e43ec1d89cc
AbdeIkader <abdelrahmanabdelkader2002@gmail.com>
2025-11-12 04:22:26 +0200

    fix: read env vars with os.environ for Railway

--- laquetha/settings.py
+++ laquetha/settings.py
@@ -51,15 +51,13 @@ INSTALLED_APPS = [
 
 DATABASES = {
     'default': {
-      'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default='5432'),
-        'OPTIONS': {
-            'sslmode': config('DB_SSLMODE', default='require'),
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'OPTIONS': {'sslmode': os.environ.get('DB_SSLMODE', 'require')},
     }
 }
 
